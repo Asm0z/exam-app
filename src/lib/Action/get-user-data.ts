8d7ef105061a32e60cfdeb81cc79f4cb43d2e0f6
Asm0z <asm0z.salama@gmail.com>
2025-12-14 18:07:02 +0200

    third commit

--- src/lib/Action/get-user-data.ts
+++ src/lib/Action/get-user-data.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 
 export async function getUserData(){
     const cookiesData = cookies()
-    const encryptedToken = cookiesData.get('next-auth.session-token')?.value
+    const encryptedToken = cookiesData.get('__Secure-next-auth.session-token')?.value
     const data = await decode({token: encryptedToken, secret: process.env.NEXTAUTH_SECRET!})
     return data
 }
