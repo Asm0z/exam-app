13c6e6f11e3795f73db0454ed7e9e1d735e75de0
Asm0z <asm0z.salama@gmail.com>
2025-12-14 17:20:42 +0200

    second commit

--- src/app/(auth)/error.tsx
+++ src/app/(auth)/error.tsx
@@ -8,7 +8,7 @@ type ErrorProps = {
 export default function Error({ error, reset }: ErrorProps) {
   return (
     <div className="flex flex-col justify-center items-center text-center">
-      <h2 className="text-2xl">An error occurred, please try again</h2>
+      <h2 className="text-2xl my-2">An error occurred, please try again</h2>
       <p className="text-xl text-red-500">{error.message}</p>
       <Button onClick={reset}>Try again</Button>
     </div>
--- src/app/error.tsx
+++ src/app/error.tsx
@@ -9,7 +9,7 @@ export default function Error({ error, reset }: ErrorProps) {
   return (
     <div className="flex flex-col justify-center items-center text-center">
       <h2 className="text-2xl">An error occurred, please try again</h2>
-      <p className="text-xl text-red-500">{error.message}</p>
+      <p className="text-xl text-red-500 my-2">{error.message}</p>
       <Button onClick={reset}>Try again</Button>
     </div>
   );
